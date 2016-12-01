7c0d1aef510252f8e6d79c2b987fc42d353f3de5
JT <jsoccerbro52@gmail.com>
2016-11-30 20:24:04 -0500

    Commented Out Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTeleopTank_Iterative.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTeleopTank_Iterative.java
@@ -54,7 +54,7 @@ import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Pushbot: Teleop Tank", group="Pushbot")
+//@TeleOp(name="Pushbot: Teleop Tank", group="Pushbot")
 //@Disabled
 public class PushbotTeleopTank_Iterative extends OpMode{
 
